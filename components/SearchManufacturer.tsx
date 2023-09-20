2cf5894396aebfee6282767d9605b92c35511fcf
Miguel Silva <miguelsilvavianaa@gmail.com>
2023-09-20 14:57:10 -0300

    create show more and finish project

--- components/SearchManufacturer.tsx
+++ components/SearchManufacturer.tsx
@@ -52,7 +52,7 @@ const SearchManufacturer = ({
             afterLeave={() => setQuery('')} // Reset the search query after the transition completes
           >
             <Combobox.Options
-              className='absolute w-full py-1 mt-1 overflow-auto text-base bg-white rounded-md shadow-lg max-h-60 ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm'
+              className='absolute z-10 w-full py-1 mt-1 overflow-auto text-base bg-white rounded-md shadow-lg max-h-60 ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm'
               static>
               {filteredManufacturers.length === 0 && query !== '' ? (
                 <Combobox.Option
